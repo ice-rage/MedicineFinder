a0b01e514165e047ae9d07261f6eeede8b316940
ice-rage <otulissa2012@yandex.ru>
2024-05-01 21:21:01 +0700

    Активация кнопки поиска при вводе текста в поисковое поле

--- medicinefinder.client/src/components/SearchForm.vue
+++ medicinefinder.client/src/components/SearchForm.vue
@@ -2,10 +2,11 @@
   <form class="search-form">
     <label class="search-form__textbox-wrapper">
       <input
+        ref="textbox"
         type="text"
         class="search-form__textbox"
         placeholder=""
-        @input="activateSearchBtn()"
+        @keyup="isRequestEmpty = !/\S/.test(textbox.value)"
       />
       <span class="search-form__textbox-placeholder">
         Введите название...
@@ -17,7 +18,9 @@
       type="submit"
       spellcheck="false"
       title="Искать" 
-      class="search-form__search-btn search-btn"
+      :class="isRequestEmpty 
+        ? 'search-form__search-btn search-btn' 
+        : 'search-form__search-btn search-btn search-btn--active'"
     >
       <div class="search-btn__circle"></div>
       <span class="search-btn__stick"></span>
@@ -28,11 +31,9 @@
 <script setup>
   import { ref } from "vue";
 
+  const textbox = ref();
+  const isRequestEmpty = ref(true);
   const searchBtn = ref();
-
-  const activateSearchBtn = () => {
-    searchBtn.value.classList.add("search-btn--active");
-  }
 </script>
 
 <style lang="less">
@@ -53,7 +54,7 @@
       box-sizing: border-box;
       display: flex;
       align-items: center;
-      padding: 0 15px;
+      padding: 0 10px 0 15px;
       outline: 0;
       border: 2px solid @islamic_green;
       border-right: 0;
@@ -117,88 +118,94 @@
     max-width: 40px;
     padding: 0;
     border-radius: 0 4px 4px 0;
-    background: rgba(@indian_green, 1);
+    background-color: @indian_green;
+    background: fade(@indian_green, 60%);
     transition: all 0.3s ease-in-out;
+    pointer-events: none;
 
     @media @bw768 {
       max-width: 30px;
     }
+    
+    &--active {
+      background: rgba(@indian_green, 1);
+      pointer-events: auto;
 
-    &::before {
-      content: "";  
-      position: absolute;
-      top: 0; 
-      left: 0;  
-      display: block;
-      height: 100%;
-      width: 100%;
-      border-radius: inherit;
-      background: radial-gradient(ellipse at left, @light_lime, @indian_green);
-      z-index: -100;
-    }
+      &::before {
+        content: "";  
+        position: absolute;
+        top: 0; 
+        left: 0;  
+        display: block;
+        height: 100%;
+        width: 100%;
+        border-radius: inherit;
+        background: radial-gradient(ellipse at left, @light_lime, @indian_green);
+        z-index: -100;
+      }
 
-    &--active,
-    &:hover {
-      @media(hover: hover) {
-        background: rgba(255, 0, 0, 0);
+      &:hover {
+        @media(hover: hover) {
+          background: rgba(255, 0, 0, 0);
 
-        &::before {
-          content: "";
-          position: absolute; 
-          top: 0; 
-          left: 0;
-          width: 100%;
-          height: 100%;
-          border-radius: inherit;
-        }
-
-        .search-btn__circle {
-          top: 1px;
-          left: 6px;
-          width: 20px;
-          height: 4px;
-          border-color: transparent;
-          background-color: @dandelion;
-          border-radius: 16px;
-
-          @media @bw768 {
-            left: 5px;
-            width: 15px;
-            height: 3px;
-            border-radius: 12px;
+          &::before {
+            content: "";
+            position: absolute; 
+            top: 0; 
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border-radius: inherit;
           }
-        }
 
-        .search-btn__stick {
-          top: 45%;
-          margin-top: -7px;
-          transform: rotateZ(0);
+          .search-btn__circle {
+            top: 1px;
+            left: 6px;
+            width: 20px;
+            height: 4px;
+            border-color: transparent;
+            background-color: @dandelion;
+            border-radius: 16px;
 
-          @media @bw768 {
-            margin-top: -5px;
+            @media @bw768 {
+              left: 5px;
+              width: 15px;
+              height: 3px;
+              border-radius: 12px;
+            }
           }
-        }
 
-        .search-btn__stick:before,
-        .search-btn__stick:after {
-          background-color: @dandelion;
-        }
+          .search-btn__stick {
+            top: 45%;
+            margin-top: -7px;
+            transform: rotateZ(0);
 
-        .search-btn__stick:before {
-          bottom: 4px;
-          transform: rotateZ(60deg);
-
-          @media @bw768 {
-            bottom: 3px;
+            @media @bw768 {
+              margin-top: -5px;
+            }
           }
-        }
 
-        .search-btn__stick:after {
-          bottom: -4px;
-          transform: rotateZ(-60deg);
+          .search-btn__stick:before,
+          .search-btn__stick:after {
+            background-color: @dandelion;
+          }
 
-          @media @bw768 {
-            bottom: -3px;
+          .search-btn__stick:before {
+            bottom: 4px;
+            transform: rotateZ(60deg);
+
+            @media @bw768 {
+              bottom: 3px;
+            }
+          }
+
+          .search-btn__stick:after {
+            bottom: -4px;
+            transform: rotateZ(-60deg);
+
+            @media @bw768 {
+              bottom: -3px;
+            }
           }
         }
       }
