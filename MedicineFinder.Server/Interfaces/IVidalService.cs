f11c82aacfab3f406b6ef6ed6631f8ee62261fa0
ice-rage <otulissa2012@yandex.ru>
2024-05-03 21:59:40 +0700

    Реализована IHttpFactory для вызова API справочника препаратов

---
+++ MedicineFinder.Server/Interfaces/IVidalService.cs
@@ -0,0 +1,9 @@
+﻿using MedicineFinder.Server.Models;
+
+namespace MedicineFinder.Server.Interfaces
+{
+    public interface IVidalService
+    {
+        Task<Root?> GetMedicineInfo(string medicineName);
+    }
+}
---
+++ MedicineFinder.Server/Services/VidalService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using MedicineFinder.Server.Interfaces;
+using MedicineFinder.Server.Models;
+
+namespace MedicineFinder.Server.Services
+{
+    public class VidalService : IVidalService
+    {
+        private readonly HttpClient _httpClient;
+
+        public VidalService(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async Task<Root?> GetMedicineInfo(string medicineName)
+        {
+            var response = await _httpClient.GetAsync(
+                $"/api/rest/v1/product/list?filter[name]={medicineName}");
+            response.EnsureSuccessStatusCode();
+
+            var stringResult = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<Root>(stringResult);
+        }
+    }
+}
